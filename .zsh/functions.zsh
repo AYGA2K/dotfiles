49168de4d3f1bfe6471d3fd32fe548c83753872b
AYGA2k <garouatayoub@gmail.com>
2024-01-24 00:30:44 +0100

    update

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -21,6 +21,9 @@ sourcezsh(){
 
 
 backupdotfiles() {
+  if [ -z "$1" ]; then
+    echo "Usage: backup_dotfiles <commit_message>"
+  else
    CURR_DIR=$(pwd)
    DIR=~/hyprland_dotfiles
    # Create the directory if it doesn't exist
@@ -30,12 +33,18 @@ backupdotfiles() {
    cp ~/.wezterm.lua $DIR
 
      # Copy .config
-   cp -r ~/.config  $DIR/
+   cp -r ~/.config/hypr ~/.config/qt6ct ~/.config/qt5ct ~/.config/tmux ~/.config/swaylock ~/.config/wofi ~/.config/wlogout ~/.config/alacritty ~/.config/nvim ~/.config/mpv ~/.config/zathura  $DIR/
 
     # Copy .zshrc
    cp ~/.zshrc $DIR
    cp -r ~/.zsh  $DIR
 
     # installed packages
-   yay -Qqe  > $DIR/packages.txt
+   # yay -Qqe  > $DIR/packages.txt
+
+    # git
+   cd $DIR
+   git add . && git commit -m "$1" && git push origin main
+   cd $CURR_DIR
+ fi
 }
