5536fa16eb71edbc2a73da6cd644ee40179ceac9
AYGA2k <garouatayoub@gmail.com>
2024-02-08 17:29:04 +0100

    remove zsh and add fish/starship

--- .config/fish/functions/backupdotfiles.fish
+++ .config/fish/functions/backupdotfiles.fish
@@ -12,7 +12,7 @@ function backupdotfiles
         mkdir -p $DIR/.config
         cp -r ~/.config/hypr ~/.config/qt6ct ~/.config/qt5ct ~/.config/tmux ~/.config/swaylock ~/.config/wofi ~/.config/wlogout ~/.config/alacritty ~/.config/nvim ~/.config/mpv ~/.config/zathura ~/.config/swayimg ~/.config/fish $DIR/.config/
 
-        cp ~/.config/starship.toml $DIR/starship.toml
+        cp ~/.config/starship.toml $DIR/.config/starship.toml
 
         cd $DIR
         git add . && git commit -m "$argv[1]" && git push origin main
