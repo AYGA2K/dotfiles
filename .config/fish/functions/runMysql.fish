a495a1ef60563b4a5bdea6111844beac2ec68e38
AYGA2k <garouatayoub@gmail.com>
2024-03-29 23:24:45 +0000

    update

--- .config/fish/functions/runMysql.fish
+++ .config/fish/functions/runMysql.fish
@@ -1,13 +1,14 @@
 function runMysql
     if test -z "$argv[1]"
-        echo "Usage: run_mysql <password>"
-        return 1
+        set password "password"
+    else
+        set password $argv[1]
     end
 
     docker run -d -p 3306:3306 \
         --name mysql \
-        -e MYSQL_ROOT_PASSWORD="$argv[1]" \
+        -e MYSQL_ROOT_PASSWORD="$password" \
         mysql
 
-    echo "MySQL container started. Username: root, Password: $argv[1]"
+    echo "MySQL container started. Username: root, Password: $password"
 end
