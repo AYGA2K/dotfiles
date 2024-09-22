3a0ffb31e4c0f05c82e36209a4d946b6dc301889
AYGA2K <garouatayoub@gmail.com>
2024-09-22 07:50:42 +0100

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,24 +9,9 @@ return {
     },
   },
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      on_colors = function(colors)
-        colors.bg_statusline = "NONE"
-      end,
-    },
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-moon",
+      colorscheme = "catppuccin",
     },
   },
 }
