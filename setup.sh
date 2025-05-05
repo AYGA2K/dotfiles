82e5153a8fb536e0afffa058fbcf3b85448a9b4d
AYGA2K <garouatayoub@gmail.com>
2025-05-05 20:16:10 +0400

    update

--- setup.sh
+++ setup.sh
@@ -91,7 +91,7 @@ packages=(
   brightnessctl playerctl network-manager-applet hyprpicker xdg-desktop-portal-hyprland
   hyprsysteminfo hyprsunset hyprpolkitagent hyprland-qt-support hyprcursor
   hyprutils hyprlang hyprwayland-scanner aquamarine hyprgraphics hyprland-qtutils
-  ags-hyprpanel-git hyprswitch cliphist go npm zellij nemo nemo-fileroller pulsemixer nwg-look kvantum qbittorrent
+  ags-hyprpanel-git hyprswitch cliphist go npm zellij nemo nemo-fileroller pulsemixer nwg-look kvantum qbittorrent yazi
 )
 
 msg "$BLUE" "[→] Installing Hyprland packages..."
