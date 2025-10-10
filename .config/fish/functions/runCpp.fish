bf4f9395c81caf3e2d7d6e8b7fa65448bd178641
AYGA2K <garouatayoub@gmail.com>
2025-10-10 21:54:32 +0400

    update

--- .config/fish/functions/runCpp.fish
+++
@@ -1,9 +0,0 @@
-function runCpp
-    if test -z "$argv[1]"
-        echo "Usage: runCpp <filename.cpp>"
-    else
-        # Get the filename without the .cpp extension
-        set name (basename $argv[1] .cpp)
-        g++ -o $name $argv[1]; and ./$name
-    end
-end
